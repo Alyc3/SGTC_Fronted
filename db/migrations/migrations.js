dcdb62037998fce98152cdd2852e1825fc75a2aa
Alyce Maldonado <alyce.maldonado@unl.edu.ec>
2026-06-07 01:24:20 -0500

    Fix migration conflicts and sequence in migrations.js and _journal.json

--- db/migrations/migrations.js
+++ db/migrations/migrations.js
@@ -10,21 +10,22 @@ import m0005 from './0005_next_weapon_omega.sql';
 import m0006 from './0006_violet_naoko.sql';
 import m0007 from './0007_mighty_moira_mactaggert.sql';
 import m0008 from './0008_careful_namora.sql';
-import m0009 from './0009_icy_steve_rogers.sql';
+import m0009 from './0009_add_cosecha_schema.sql';
+import m0010 from './0010_icy_steve_rogers.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001,
-m0002,
-m0003,
-m0004,
-m0005,
-m0006,
-m0007,
-m0008,
-m0009
+      m0001,
+      m0002,
+      m0003,
+      m0004,
+      m0005,
+      m0006,
+      m0007,
+      m0008,
+      m0009,
+      m0010
     }
   }
-  
